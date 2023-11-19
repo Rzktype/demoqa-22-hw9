755a265f55715eee8ba0668e93664b01e814561b
Maxim Nikiforov <rezusjkee@mail.ru>
2023-11-19 22:19:57 +0500

    add steps

--- src/test/java/DemoQaFormTests.java
+++ src/test/java/DemoQaFormTests.java
@@ -1,7 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.UserInfoPage;
 import pages.components.ChecksOutputInfoComponent;
 import utils.RandomUtils;
+
+import static io.qameta.allure.Allure.step;
 
 
 public class DemoQaFormTests extends tests.TestBase {
@@ -10,32 +14,40 @@ public class DemoQaFormTests extends tests.TestBase {
     RandomUtils random = new RandomUtils();
 
     @Test
+    @Tag("JenkinsDemoQA")
+    @DisplayName("Successfull Registration")
     void fillFormTest() {
 
-        userInfoPage.openPage()
-                .setFirstName(random.firstName)
-                .setLastName(random.lastName)
-                .setUserEmail(random.email)
-                .setGender(random.gender)
-                .setUserNumber(random.phone)
-                .setDateOfBirth(random.dayOfBirth, random.monthOfBirth, random.yearOfBirth)
-                .setSubjectsInput(random.subjects)
-                .setHobbies(random.hobbies)
-                .uploadPicture(random.picture)
-                .setAddress(random.address)
-                .setState(random.chooseState)
-                .setCity(random.chooseCity)
-                .clickSubmit();
+        step("Open registration form", () -> {
+            userInfoPage.openPage();
+        });
+        step("Fill user info and click submit", () -> {
+            userInfoPage.setFirstName(random.firstName)
+                    .setLastName(random.lastName)
+                    .setUserEmail(random.email)
+                    .setGender(random.gender)
+                    .setUserNumber(random.phone)
+                    .setDateOfBirth(random.dayOfBirth, random.monthOfBirth, random.yearOfBirth)
+                    .setSubjectsInput(random.subjects)
+                    .setHobbies(random.hobbies)
+                    .uploadPicture(random.picture)
+                    .setAddress(random.address)
+                    .setState(random.chooseState)
+                    .setCity(random.chooseCity)
+                    .clickSubmit();
+        });
+        step("Verify results", () -> {
+            tableUserInfo.checkResult("Student Name", random.firstName + " " + random.lastName)
+                    .checkResult("Student Email", random.email)
+                    .checkResult("Gender", random.gender)
+                    .checkResult("Mobile", random.phone)
+                    .checkResult("Date of Birth", random.dayOfBirth + " " + random.monthOfBirth + "," + random.yearOfBirth)
+                    .checkResult("Subjects", random.subjects)
+                    .checkResult("Hobbies", random.hobbies)
+                    .checkResult("Picture", random.picture)
+                    .checkResult("Address", random.address)
+                    .checkResult("State and City", random.chooseState + " " + random.chooseCity);
 
-        tableUserInfo.checkResult("Student Name", random.firstName + " " + random.lastName)
-                .checkResult("Student Email", random.email)
-                .checkResult("Gender", random.gender)
-                .checkResult("Mobile", random.phone)
-                .checkResult("Date of Birth", random.dayOfBirth + " " + random.monthOfBirth + "," + random.yearOfBirth)
-                .checkResult("Subjects", random.subjects)
-                .checkResult("Hobbies", random.hobbies)
-                .checkResult("Picture", random.picture)
-                .checkResult("Address", random.address)
-                .checkResult("State and City", random.chooseState + " " + random.chooseCity);
+        });
     }
 }
--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,24 +1,34 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.components.ChecksOutputInfoComponent;
+
+import static io.qameta.allure.Allure.step;
 
 public class TextBoxTests extends tests.TestBase {
 
 
     @Test
+    @Tag("JenkinsTextBox")
+    @DisplayName("Successfull Registration")
     void fillFormTest() {
         ChecksOutputInfoComponent checkOutput = new ChecksOutputInfoComponent();
 // test
-        textBoxPage.openPage()
-                .setUserName("Dmitry Volkov")
-                .setUserEmail("DmitryVolkov@mail.ru")
-                .setCurrentAddress("Moscow")
-                .setPermanentAddress("86 Tatishcheva str., Yekaterinburg, Sverdlovsk region, 620028")
-                .clickButton();
+        step("Open and fill form", () -> {
+            textBoxPage.openPage()
+                    .setUserName("Dmitry Volkov")
+                    .setUserEmail("DmitryVolkov@mail.ru")
+                    .setCurrentAddress("Moscow")
+                    .setPermanentAddress("86 Tatishcheva str., Yekaterinburg, Sverdlovsk region, 620028")
+                    .clickButton();
+        });
 // checks
-        checkOutput.checkOutputResult("Name", "Dmitry Volkov")
-                .checkOutputResult("Email", "DmitryVolkov@mail.ru")
-                .checkOutputResult("Current Address", "Moscow")
-                .checkOutputResult("Permananet Address", "Yekaterinburg");
+        step("Check Results", () -> {
+            checkOutput.checkOutputResult("Name", "Dmitry Volkov")
+                    .checkOutputResult("Email", "DmitryVolkov@mail.ru")
+                    .checkOutputResult("Current Address", "Moscow")
+                    .checkOutputResult("Permananet Address", "Yekaterinburg");
+        });
 
 
     }
